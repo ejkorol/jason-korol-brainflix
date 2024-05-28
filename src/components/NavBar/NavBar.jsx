5b1d1609911749bed8cf299550236ad967cf0323
Jason Korol <ejkorol@gmail.com>
2024-05-28 15:42:51 -0400

    import search component into navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,14 @@
 import './NavBar.scss';
 
-import Logo from "../Logo/Logo.jsx";
+import Logo from "../Logo/Logo";
+import Search from "../Search/Search";
 
 function NavBar() {
 
   return (
     <header>
       <Logo />
+      <Search />
     </header>
   )
 
