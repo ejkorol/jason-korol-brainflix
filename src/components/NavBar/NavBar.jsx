907c50df99b005e89538c33f051be1060389e258
Jason Korol <ejkorol@gmail.com>
2024-05-29 12:49:01 -0400

    pass props to button

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,13 @@
 import './NavBar.scss';
 
+/* COMPONENTS */
 import Logo from "@/components/Logo/Logo";
 import Search from "@/components/Search/Search";
 import Button from "@/components/Button/Button";
 import Avatar from "@/components/Avatar/Avatar";
+
+/* ICONS */
+import uploadIcon from "@/assets/icons/upload.svg";
 
 function NavBar() {
 
@@ -11,7 +15,7 @@ function NavBar() {
     <header>
       <Logo />
       <Search />
-      <Button />
+      <Button icon={uploadIcon} text="upload" />
       <Avatar />
     </header>
   )
