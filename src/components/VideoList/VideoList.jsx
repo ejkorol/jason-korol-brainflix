93b490a553fc427d860ce8cc73381cf52444d041
Jason Korol <ejkorol@gmail.com>
2024-05-31 15:49:05 -0400

    import VideoSnippet component into VideoList

--- src/components/VideoList/VideoList.jsx
+++ src/components/VideoList/VideoList.jsx
@@ -1,11 +1,14 @@
 /* STYLES */
 import "./VideoList.scss";
 
+/* COMPONENTS */
+import VideoSnippet from "@/components/VideoSnippet/VideoSnippet";
+
 function VideoList() {
   return (
-    <>
-      ...
-    </>
+    <section className="video-list">
+      <VideoSnippet />
+    </section>
   )
 };
 
