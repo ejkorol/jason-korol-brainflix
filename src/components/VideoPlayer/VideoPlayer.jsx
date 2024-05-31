9ee90dc21bc5a3207267c2c870736f2c64be553d
Jason Korol <ejkorol@gmail.com>
2024-05-31 12:38:59 -0400

    import VideoComments component into VideoPlayer

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -3,12 +3,14 @@ import "./VideoPlayer.scss";
 /* COMPONENTS */
 import Video from "@/components/Video/Video";
 import VideoDetails from "@/components/VideoDetails/VideoDetails";
+import VideoComments from "@/components/VideoComments/VideoComments";
 
 function VideoPlayer () {
   return (
     <section className="video-player">
       <Video />
       <VideoDetails />
+      <VideoComments />
     </section>
   )
 };
