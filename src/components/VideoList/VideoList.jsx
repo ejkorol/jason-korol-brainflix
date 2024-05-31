75147d5e89e16089d94ab413fac945994a32e178
Jason Korol <ejkorol@gmail.com>
2024-05-31 16:43:32 -0400

    add initial style to VideoList component

--- src/components/VideoList/VideoList.jsx
+++ src/components/VideoList/VideoList.jsx
@@ -7,6 +7,7 @@ import VideoSnippet from "@/components/VideoSnippet/VideoSnippet";
 function VideoList() {
   return (
     <section className="video-list">
+      <h3 className="video-list__header">next videos</h3>
       <VideoSnippet />
     </section>
   )
