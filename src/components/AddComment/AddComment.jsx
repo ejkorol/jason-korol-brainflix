7da588cf52a159ca8187a23657aa4e57a9a3b62b
Jason Korol <ejkorol@gmail.com>
2024-05-31 14:06:48 -0400

    add initial style for AddComment component

--- src/components/AddComment/AddComment.jsx
+++ src/components/AddComment/AddComment.jsx
@@ -1,11 +1,25 @@
 /* STYLES */
 import "./AddComment.scss";
 
+/* ICONS */
+import addCommentIcon from "@/assets/icons/add_comment.svg";
+
+/* COMPONENTS */
+import Button from "@/components/Button/Button";
+import Avatar from "@/components/Avatar/Avatar";
+
 function AddComment() {
   return (
-    <>
-      ...
-    </>
+    <div className="add-comment">
+      <div className="add-comment__image-wrapper">
+        <Avatar />
+      </div>
+      <form className="add-comment__form">
+        <label className="add-comment__label">join the conversation</label>
+        <textarea className="add-comment__textarea" placeholder="Add a new comment" rows="5"></textarea>
+        <Button icon={addCommentIcon} text="comment" />
+      </form>
+    </div>
   )
 };
 
