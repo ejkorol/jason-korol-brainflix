46af08a4ad8bc99f33478fb53b9329452e548f8a
Jason Korol <ejkorol@gmail.com>
2024-05-31 13:00:19 -0400

    import Comment component files

--- src/components/VideoComments/VideoComments.jsx
+++ src/components/VideoComments/VideoComments.jsx
@@ -1,11 +1,17 @@
 /* STYLES */
 import "./VideoComments.scss";
 
+/* COMPONENTS */
+import AddComment from "@/components/AddComment/AddComment";
+import CommentsFeed from "@/components/CommentsFeed/CommentsFeed";
+
 function VideoComments() {
   return (
-    <>
-      ...
-    </>
+    <section>
+      <p>3 Comments</p>
+      <AddComment />
+      <CommentsFeed />
+    </section>
   )
 };
 
