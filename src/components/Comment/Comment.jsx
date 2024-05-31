a838c394c0e208b7b5a3a893892df23e134db621
Jason Korol <ejkorol@gmail.com>
2024-05-31 14:35:03 -0400

    add initial style for Comment component

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -1,10 +1,24 @@
 /* STYLES */
 import "./Comment.scss";
 
+/* COMPONENTS */
+import Avatar from "@/components/Avatar/Avatar";
+
 function Comment() {
   return (
-    <article>
-      ...
+    <article className="comment">
+      <div className="comment__image-wrapper">
+        <Avatar />
+      </div>
+      <div className="comment__content">
+        <div className="comment__top">
+          <p className="comment__text comment__text--bold">Jesus C</p>
+          <p className="comment__text comment__text--light">8/11/2022</p>
+        </div>
+        <div className="comment__body">
+          <p className="comment__text">Every we stray further from gods light. Every we stray further from gods light. Every we stray further from gods light. Every we stray further from gods light.</p>
+        </div>
+      </div>
     </article>
   )
 };
