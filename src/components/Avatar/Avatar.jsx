8b30a8b8e8337115934f6ba890f1c086d9fe42a0
Jason Korol <ejkorol@gmail.com>
2024-06-01 12:40:47 -0400

    refactor avatar style for reusability

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -3,7 +3,7 @@ import "./Avatar.scss";
 
 function Avatar (props) {
   return (
-    <img src={props.image} alt={props.image ? "User Avatar" : null } className={props.image ? "header__avatar" : "header__avatar header__avatar--placeholder"} />
+    <img src={props.image} alt={props.image ? "User Avatar" : null } className={props.image ? "avatar" : "avatar avatar--placeholder"} />
   )
 };
 
