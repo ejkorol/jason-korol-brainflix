8915b08c13433d60a6483a1e381b53442a2b9684
Jason Korol <ejkorol@gmail.com>
2024-06-02 18:52:41 -0400

    setting state through event handler

--- src/components/VideoList/VideoList.jsx
+++ src/components/VideoList/VideoList.jsx
@@ -4,18 +4,19 @@ import "./VideoList.scss";
 /* COMPONENTS */
 import VideoSnippet from "@/components/VideoSnippet/VideoSnippet";
 
-function createSnippet(video) {
-  return <VideoSnippet key={video.id} image={video.image} title={video.title} author={video.channel} />
-};
-
 function VideoList({ videoList, setVideo }) {
 
-  const videos = videoList;
+  function handleVideoChange(video) {
+    console.log(video);
+    setVideo(video);
+  };
 
   return (
     <section className="video-list">
       <h3 className="video-list__header">next videos</h3>
-      {videos.map(createSnippet)}
+      {videoList.map(video => {
+        return <VideoSnippet onClick={() => handleVideoChange(video)} key={video.id} image={video.image} title={video.title} author={video.channel} id={video.id} />
+      })}
     </section>
   )
 };
