04eee9d6cce48b7fefd720ccca2b9b2edde68bb1
Jason Korol <ejkorol@gmail.com>
2024-06-02 14:55:02 -0400

    remapping VideoDetails component

--- src/components/VideoDetails/VideoDetails.jsx
+++ src/components/VideoDetails/VideoDetails.jsx
@@ -7,29 +7,28 @@ import eyeIcon from "@/assets/icons/views.svg";
 
 function VideoDetails () {
   return (
-    <section className="video-player__content">
-
-      <div className="video-player__wrapper">
-        <h1 className="video-player__header">The Future of Artifical Intelligence</h1>
+    <>
+      <div className="video-details__title">
+        <h1 className="video-details__header">The Future of Artifical Intelligence</h1>
       </div>
 
-      <div className="video-content">
-        <div className="video-content__wrapper">
-          <div className="video-content__author">
-            <p className="video-content__text video-content__text--bold">By Aiden Thompson</p>
-            <p className="video-content__text video-content__text--light">8/8/2023</p>
+      <div className="video-details__content">
+        <div className="video-details__wrapper">
+          <div className="video-details__author">
+            <p className="video-details__text video-details__text--bold">By Aiden Thompson</p>
+            <p className="video-details__text video-details__text--light">8/8/2023</p>
           </div>
 
-          <div className="video-content__info">
-            <p className="video-content__text video-content__text--light">
-              <span className="video-content__text--icon">
+          <div className="video-details__info">
+            <p className="video-details__text video-details__text--light">
+              <span className="video-details__text--icon">
                 <img src={heartIcon} alt="likes icon" />
               </span>
               980,544
             </p>
 
-            <p className="video-content__text video-content__text--light">
-              <span className="video-content__text--icon">
+            <p className="video-details__text video-details__text--light">
+              <span className="video-details__text--icon">
                 <img src={eyeIcon} alt="views icon" />
               </span>
               980,544
@@ -37,12 +36,11 @@ function VideoDetails () {
           </div>
         </div>
 
-        <div className="video-content__wrapper video-content__wrapper--no-border">
-          <p className="video-content__text">Explore the cutting-edge developments and predictions for Artificial Intelligence in the coming years. From revolutionary breakthroughs in machine learning to the ethical considerations influencing AI advancements, this exploration transcends the boundaries of mere speculation. Join us on a journey that navigates the intricate interplay between innovation, ethics, and the ever-evolving tech frontier.</p>
+        <div className="video-details__wrapper video-details__wrapper--no-border">
+          <p className="video-details__text">Explore the cutting-edge developments and predictions for Artificial Intelligence in the coming years. From revolutionary breakthroughs in machine learning to the ethical considerations influencing AI advancements, this exploration transcends the boundaries of mere speculation. Join us on a journey that navigates the intricate interplay between innovation, ethics, and the ever-evolving tech frontier.</p>
         </div>
       </div>
-
-    </section>
+    </>
   )
 };
 
