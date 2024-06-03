04ea743035437e1c2c8c43169f4d6d68a5dcd81b
Jason Korol <ejkorol@gmail.com>
2024-06-02 22:21:20 -0400

    add submit handler on AddComment form

--- src/components/AddComment/AddComment.jsx
+++ src/components/AddComment/AddComment.jsx
@@ -10,18 +10,23 @@ import Button from "@/components/Button/Button";
 import Avatar from "@/components/Avatar/Avatar";
 
 function AddComment() {
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  };
+
   return (
     <div className="add-comment">
       <div className="add-comment__image-wrapper">
         <Avatar image={userAvatar} />
       </div>
-      <form className="add-comment__form">
+      <form className="add-comment__form" onSubmit={handleSubmit}>
         <div className="add-comment__wrapper add-comment__wrapper--textarea">
           <label className="add-comment__label">join the conversation</label>
           <textarea className="add-comment__textarea" placeholder="Add a new comment"></textarea>
         </div>
         <div className="add-comment__wrapper add-comment__wrapper--button">
-          <Button icon={addCommentIcon} text="comment" />
+          <Button icon={addCommentIcon} text="comment" type="submit" />
         </div>
       </form>
     </div>
