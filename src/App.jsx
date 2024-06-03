d1913fac01896299a673e4d4685f6dc2fd094b3c
Jason Korol <ejkorol@gmail.com>
2024-06-03 15:38:27 -0400

    add router config to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,50 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+/* STYLES */
 import "./App.scss";
 
-import Home from "@/pages/Home/Home";
+/* ROUTES */
+import HomePage from "@/pages/HomePage/HomePage";
+import UploadPage from "@/pages/UploadPage/UploadPage";
+import VideoPage from "@/pages/VideoPage/VideoPage";
+import NotFoundPage from "@/pages/NotFoundPage/NotFoundPage";
+
+/* ROUTE CONFIG */
+const routes = [
+  {
+    name: 'Home',
+    path: '/',
+    component: () => <HomePage />
+  },
+  {
+    name: 'Upload',
+    path: 'upload',
+    component: () => <UploadPage />
+  },
+  {
+    name: 'Video',
+    path: 'video/:id',
+    component: () => <VideoPage />
+  },
+  {
+    name: '404',
+    path: '*',
+    component: () => <NotFoundPage />
+  }
+];
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home/>} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        {routes.map((route) => <Route
+          key={route.name}
+          path={route.path}
+          element={route.component()}
+        />)}
+      </Routes>
+    </BrowserRouter>
   )
-}
+};
 
-export default App
+export default App;
