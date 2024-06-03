8166591e436f2107ab69fad4fdf8109d2685153d
Jason Korol <ejkorol@gmail.com>
2024-06-03 14:23:24 -0400

    add Home link in NavBar logo

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,9 @@
+import { Link } from "react-router-dom";
+
 /* STYLES */
 import "./NavBar.scss";
 import userAvatar from "@/assets/images/Mohan-muruge.jpg"
+import uploadIcon from "@/assets/icons/upload.svg";
 
 /* COMPONENTS */
 import Logo from "@/components/Logo/Logo";
@@ -8,14 +11,13 @@ import Search from "@/components/Search/Search";
 import Button from "@/components/Button/Button";
 import Avatar from "@/components/Avatar/Avatar";
 
-/* ICONS */
-import uploadIcon from "@/assets/icons/upload.svg";
-
 function NavBar() {
 
   return (
     <header className="header">
-      <Logo />
+      <Link to="/">
+        <Logo />
+      </Link>
       <Search />
       <Button icon={uploadIcon} text="upload" />
       <Avatar image={userAvatar} />
