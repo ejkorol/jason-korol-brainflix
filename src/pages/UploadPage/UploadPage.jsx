f27560d3eda17b3c3de67acbe9594b6e3d1c8ba6
Jason Korol <ejkorol@gmail.com>
2024-06-03 17:16:04 -0400

    add initial style for UploadPage

--- src/pages/UploadPage/UploadPage.jsx
+++ src/pages/UploadPage/UploadPage.jsx
@@ -1,10 +1,46 @@
+/* STYLES */
 import "./UploadPage.scss";
+import publishIcon from "@/assets/icons/publish.svg";
+
+/* COMPONENTS */
+import Button from "@/components/Button/Button";
 
 function UploadPage() {
   return (
-    <>
-      ...
-    </>
+    <section className="upload">
+
+      <div className="upload__container">
+
+        <h1 className="upload__header">Upload Video</h1>
+
+        <div className="upload__left">
+          <label className="upload__label">video thumbnail</label>
+          <img className="upload__image" src="https://i.pinimg.com/736x/32/7e/db/327edb9a15b304efc264668ada03f725.jpg" alt="work hard everyday, make few fucking money" />
+        </div>
+
+        <div className="upload__right">
+
+          <form className="upload-form">
+            <label className="upload-form__label">title your video</label>
+            <textarea className="upload-form__input" placeholder="Add a title to your video"></textarea>
+
+            <label className="upload-form__label">add a video description</label>
+            <textarea className="upload-form__textarea" placeholder="Add a description to your video"></textarea>
+          </form>
+
+        </div>
+      </div>
+
+      <div className="upload-footer">
+        <div className="upload-footer__cancel-button">
+          <Button role="button" text="cancel" variant="light" />
+        </div>
+        <div className="upload-footer__publish-button">
+          <Button icon={publishIcon} role="button" text="publish" />
+        </div>
+      </div>
+
+    </section>
   )
 };
 
