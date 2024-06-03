a2eb5c95ac304387527edba8aefe62292b434602
Jason Korol <ejkorol@gmail.com>
2024-06-02 23:32:00 -0400

    add verboise time for diving deeper section

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -6,8 +6,34 @@ import Avatar from "@/components/Avatar/Avatar";
 
 function Comment({ name, timestamp, content }) {
 
+  /* TIMESTAMP LOGIC */
   function convertTimestamp(time) {
-    return new Date(time).toLocaleDateString('en-US');
+    const dif = Date.now() - time;
+    const seconds = Math.floor(dif / 1000);
+
+    const timeUnits = [
+      { unit: 'year', value: 31536000 },
+      { unit: 'month', value: 2592000 },
+      { unit: 'day', value: 86400 },
+      { unit: 'hour', value: 3600 },
+      { unit: 'minute', value: 60 }
+    ];
+
+    for (let i = 0; i < timeUnits.length; i++) {
+      const unit = timeUnits[i].unit;
+      const value = timeUnits[i].value;
+      const elapsed = Math.floor(seconds / value);
+
+      if (elapsed >= 1) {
+        let unitStr = unit;
+        if (elapsed > 1) {
+          unitStr = unit + 's';
+        };
+        return elapsed + ' ' + unitStr + ' ago';
+      };
+    };
+
+    return 'Just now';
   };
 
   return (
