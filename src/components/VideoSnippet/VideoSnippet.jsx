d6ba5f6591d94bccc739c6d7f65a3b07e9f63141
Jason Korol <ejkorol@gmail.com>
2024-06-04 23:25:16 -0400

    add router link to VideoSnippet component, style Link tag

--- src/components/VideoSnippet/VideoSnippet.jsx
+++ src/components/VideoSnippet/VideoSnippet.jsx
@@ -1,17 +1,21 @@
+import { Link } from "react-router-dom";
+
 /* STYLES */
 import "./VideoSnippet.scss";
 
-function VideoSnippet({ image, title, author, onClick }) {
+function VideoSnippet({ image, title, author, id }) {
   return (
-    <article className="video-snippet" onClick={onClick}>
-      <div className="video-snippet__image-wrapper">
-        <img className="video-snippet__image" src={image} alt={title} />
-      </div>
-      <div className="video-snippet__content">
-        <h3 className="video-snippet__text video-snippet__text--bold">{title}</h3>
-        <p className="video-snippet__text">{author}</p>
-      </div>
-    </article>
+    <Link to={`/video/${id}`} className="video-snippet__link">
+      <article className="video-snippet">
+        <div className="video-snippet__image-wrapper">
+          <img className="video-snippet__image" src={image} alt={title} />
+        </div>
+        <div className="video-snippet__content">
+          <h3 className="video-snippet__text video-snippet__text--bold">{title}</h3>
+          <p className="video-snippet__text">{author}</p>
+        </div>
+      </article>
+    </Link>
   )
 };
 
