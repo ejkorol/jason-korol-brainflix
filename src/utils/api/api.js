c8ec44dd78b697e63111a6ae905bb67a61ef982b
Jason Korol <ejkorol@gmail.com>
2024-06-04 20:27:27 -0400

    create class for constructing api

---
+++ src/utils/api/api.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+
+class Api {
+
+  constructor (baseURL, apiKey) {
+    this.apiKey = apiKey;
+    this.baseURL = baseURL;
+    this.api = axios.create({
+      baseURL: this.baseURL,
+      params: {
+        api_key: this.apiKey
+      },
+    });
+  };
+
+  /* GET DEFAULT VIDEO */
+  async getDefaultVideo() {
+    const videos = await this.getVideoList();;
+    const route = `/videos/${videos[1].id}`;
+    try {
+      const res = await this.api.get(route);
+      return res.data;
+    } catch (e) {
+      console.error(e);
+    };
+  };
+
+  /* GET VIDEO LIST */
+  async getVideoList() {
+    const route = "/videos";
+    try {
+      const res = await this.api.get(route);
+      return res.data;
+    } catch (e) {
+      console.error(e)
+    };
+  };
+
+};
+
+const BASE_URL = "https://unit-3-project-api-0a5620414506.herokuapp.com";
+const API_KEY = "60f458e4-0a5a-4961-ba43-73b0945485a8";
+const api = new Api(BASE_URL, API_KEY);
+
+export default api;
