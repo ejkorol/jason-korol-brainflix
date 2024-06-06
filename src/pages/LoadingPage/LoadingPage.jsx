11fd877db0334b5290d7f9421e432bc56a524d63
Jason Korol <ejkorol@gmail.com>
2024-06-06 15:43:20 -0400

    add loading page styling

---
+++ src/pages/LoadingPage/LoadingPage.jsx
@@ -0,0 +1,14 @@
+/* STYLES */
+import "./LoadingPage.scss";
+
+function LoadingPage() {
+  return (
+    <section className="loading-page">
+      <div className="loading-page__container">
+        <span className="loading-page__spinner"></span>
+      </div>
+    </section>
+  )
+};
+
+export default LoadingPage;
