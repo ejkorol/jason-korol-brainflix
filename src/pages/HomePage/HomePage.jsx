6dfae01e50f9d1376d3be3e13058d497b7f77b8b
Jason Korol <ejkorol@gmail.com>
2024-06-06 20:56:35 -0400

    removing loading state component from HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -8,12 +8,11 @@ import "./HomePage.scss";
 /* COMPONENTS */
 import VideoPlayer from "@/components/VideoPlayer/VideoPlayer";
 import VideoContent from "@/components/VideoContent/VideoContent";
-import LoadingPage from "@/pages/LoadingPage/LoadingPage";
 
 function HomePage() {
 
   const { id } = useParams();
-  const { video, videos, loading, error, getVideo, getDefaultVideo } = useVideos();
+  const { video, videos, error, getVideo, getDefaultVideo } = useVideos();
 
   useEffect(() => {
     if (id) {
@@ -23,13 +22,12 @@ function HomePage() {
     };
   }, [id]);
 
-  if (loading) return <LoadingPage />;
   if (error) return <p>There was an error: {error}</p>;
 
   return (
     <>
-      <VideoPlayer video={video} />
-      <VideoContent video={video} videoList={videos} getVideo={getVideo} />
+      { video && <VideoPlayer video={video} /> }
+      { video && <VideoContent video={video} videoList={videos} getVideo={getVideo} /> }
     </>
   )
 }
