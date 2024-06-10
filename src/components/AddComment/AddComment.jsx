4b26fee888bffae4eacffda13dad014f069f1f96
Jason Korol <ejkorol@gmail.com>
2024-06-09 21:09:10 -0400

    pass in id through parent params, conditionally apply error state to form, clear form upon submission

--- src/components/AddComment/AddComment.jsx
+++ src/components/AddComment/AddComment.jsx
@@ -1,6 +1,5 @@
 /* LOGIC */
 import { useRef } from "react";
-import { useParams } from "react-router-dom";
 import useComments from "@/utils/hooks/useComments";
 
 /* STYLES */
@@ -14,21 +13,30 @@ import addCommentIcon from "@/assets/icons/add_comment.svg";
 import Button from "@/components/Button/Button";
 import Avatar from "@/components/Avatar/Avatar";
 
-function AddComment({ getVideo }) {
+function AddComment({ video, getVideo }) {
 
-  const { id } = useParams();
+  const id = video.id;
   const { postComment } = useComments();
   const commentRef = useRef(null);
 
   function handleSubmit(e) {
     e.preventDefault();
+    const textarea = e.target.elements.comment;
     const userName = window.prompt("Enter your username: ", "");
     const comment = {
       name: userName,
       comment: commentRef.current.value
     };
-    postComment(comment);
-    getVideo(id);
+    if (userName && comment !== null) {
+      postComment(id, comment);
+      if (textarea.classList.contains("add-comment__textarea--error")) {
+        textarea.classList.remove("add-comment__textarea--error");
+      };
+      e.target.reset();
+      getVideo(id);
+    } else {
+      textarea.classList.add("add-comment__textarea--error");
+    };
   };
 
   return (
@@ -39,7 +47,7 @@ function AddComment({ getVideo }) {
       <form className="add-comment__form" onSubmit={handleSubmit}>
         <div className="add-comment__wrapper add-comment__wrapper--textarea">
           <label className="add-comment__label">join the conversation</label>
-          <textarea ref={commentRef} className="add-comment__textarea" placeholder="Add a new comment"></textarea>
+          <textarea ref={commentRef} name="comment" className="add-comment__textarea" placeholder="Add a new comment"></textarea>
         </div>
         <div className="add-comment__wrapper add-comment__wrapper--button">
           <Button icon={addCommentIcon} text="comment" type="submit" />
