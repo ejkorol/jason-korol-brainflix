2a2f35381e1827e3c1d198530231c624fa249fd6
Jason Korol <ejkorol@gmail.com>
2024-06-09 21:08:37 -0400

    remove useParams from router-dom and pass in ID through the hook

--- src/utils/hooks/useComments.js
+++ src/utils/hooks/useComments.js
@@ -1,14 +1,12 @@
 import { useState } from "react";
-import { useParams } from "react-router-dom";
 import api from "@/utils/api/api";
 
 function useComments() {
 
-  const { id } = useParams();
   const [comment, setComment] = useState(null);
   const [error, setError] = useState(null);
 
-  async function postComment(payload) {
+  async function postComment(id, payload) {
     try {
       const res = await api.postComment(id, payload);
       setComment(res.data);
