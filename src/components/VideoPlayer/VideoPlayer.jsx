765398dc978e0416272cb6b202c9829628caae2e
Jason Korol <ejkorol@gmail.com>
2024-06-16 00:15:06 -0400

    update image url to receive api url

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -1,11 +1,13 @@
 /* STYLES */
 import "./VideoPlayer.scss";
 
+const APIURL = import.meta.env.VITE_API_URL;
+
 function VideoPlayer ({ video: { video, image } }) {
 
   return (
     <section className="video-player">
-      <video controls src={video} poster={image} className="video-player__video">
+      <video controls src={video} poster={`${APIURL}${image}`} className="video-player__video">
         <source src={video} type="video/mp4" controls />
         Your browser does not support the video element.
       </video>
--- src/components/VideoSnippet/VideoSnippet.jsx
+++ src/components/VideoSnippet/VideoSnippet.jsx
@@ -4,13 +4,15 @@ import { Link } from "react-router-dom";
 /* STYLES */
 import "./VideoSnippet.scss";
 
+const APIURL = import.meta.env.VITE_API_URL;
+
 function VideoSnippet({ image, title, author, id }) {
 
   return (
     <Link to={`/video/${id}`} className="video-snippet__link">
       <article className="video-snippet">
         <div className="video-snippet__image-wrapper">
-          <img className="video-snippet__image" src={image} alt={title} />
+          <img className="video-snippet__image" src={`${APIURL}${image}`} alt={title} />
         </div>
         <div className="video-snippet__content">
           <h3 className="video-snippet__text video-snippet__text--bold">{title}</h3>
